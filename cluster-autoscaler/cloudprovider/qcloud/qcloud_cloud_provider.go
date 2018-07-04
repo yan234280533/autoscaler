0bcc90554d35611fbeb03a6a5548acc8e5333d75
dongdong <dongdong@tencent.com>
2018-07-04 18:22:56 +0800

    1.10

--- cluster-autoscaler/cloudprovider/qcloud/qcloud_cloud_provider.go
+++ cluster-autoscaler/cloudprovider/qcloud/qcloud_cloud_provider.go
@@ -26,7 +26,7 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/config/dynamic"
 	"k8s.io/autoscaler/cluster-autoscaler/utils/errors"
 	apiv1 "k8s.io/api/core/v1"
-	"k8s.io/kubernetes/plugin/pkg/scheduler/schedulercache"
+	"k8s.io/kubernetes/pkg/scheduler/schedulercache"
 )
 
 const (
@@ -43,9 +43,6 @@ type qcloudCloudProvider struct {
 
 // BuildQcloudCloudProvider builds CloudProvider implementation for QCLOUD.
 func BuildQcloudCloudProvider(qcloudManager *QcloudManager, discoveryOpts cloudprovider.NodeGroupDiscoveryOptions, resourceLimiter *cloudprovider.ResourceLimiter) (cloudprovider.CloudProvider, error) {
-	if err := discoveryOpts.Validate(); err != nil {
-		return nil, fmt.Errorf("Failed to build an qcloud cloud provider: %v", err)
-	}
 	if discoveryOpts.StaticDiscoverySpecified() {
 		return buildStaticallyDiscoveringProvider(qcloudManager, discoveryOpts.NodeGroupSpecs, resourceLimiter)
 	}
@@ -126,7 +123,8 @@ func (qcloud *qcloudCloudProvider) GetAvailableMachineTypes() ([]string, error) 
 
 // NewNodeGroup builds a theoretical node group based on the node definition provided. The node group is not automatically
 // created on the cloud provider side. The node group is not returned by NodeGroups() until it is created.
-func (qcloud *qcloudCloudProvider) NewNodeGroup(machineType string, labels map[string]string, extraResources map[string]resource.Quantity) (cloudprovider.NodeGroup, error) {
+func (qcloud *qcloudCloudProvider) NewNodeGroup(machineType string, labels map[string]string, systemLabels map[string]string,
+extraResources map[string]resource.Quantity) (cloudprovider.NodeGroup, error) {
 	return nil, cloudprovider.ErrNotImplemented
 }
 
