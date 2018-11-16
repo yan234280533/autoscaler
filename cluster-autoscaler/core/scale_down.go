aa32526f84b2a8ec69ef0c853d1802ba6eacb052
Alex Price <aprice@atlassian.com>
2018-11-16 11:12:17 +1100

    add flags to ignore daemonsets and mirror pods when calculating resource utilization of a node
    Adds the flag --ignore-daemonsets-utilization and --ignore-mirror-pods-utilization
    (defaults to false) and when enabled, factors DaemonSet and mirror pods out when
    calculating the resource utilization of a node.

--- cluster-autoscaler/core/scale_down.go
+++ cluster-autoscaler/core/scale_down.go
@@ -394,7 +394,8 @@ func (sd *ScaleDown) UpdateUnneededNodes(
 			glog.Errorf("Node info for %s not found", node.Name)
 			continue
 		}
-		utilization, err := simulator.CalculateUtilization(node, nodeInfo)
+
+		utilInfo, err := simulator.CalculateUtilization(node, nodeInfo, sd.context.IgnoreDaemonSetsUtilization, sd.context.IgnoreMirrorPodsUtilization)
 
 		if err != nil {
 			glog.Warningf("Failed to calculate utilization for %s: %v", node.Name, err)
